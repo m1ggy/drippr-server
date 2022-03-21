82a98586ff4070e8b9a60035bac96c6715aa73de
m1ggy <46958815+m1ggy@users.noreply.github.com>
2022-03-21 14:41:50 +0800

    fix conditional

--- controllers/readingController.js
+++ controllers/readingController.js
@@ -59,7 +59,7 @@ const addReading = async (req, res) => {
                 if (currentPlan) {
                     if (currentPlan.active == true) {
                         // if the parsed value is less than equals the minimum threshold of the current plan, start irrigating
-                        if (parseInt(currentPlan.threshold.min) <= parseInt(parsed.value)) {
+                        if (parseInt(currentPlan.threshold.min) >= parseInt(parsed.value)) {
                             console.log("WATERING: ", parsed.id)
                             const wateringTime = scheduler.getWateringTime(
                                 currentPlan.id,
